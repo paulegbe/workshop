8107c6ae1e384ca7ada5ddeba7fc4c9368604cf0
PaulEgbe <paulegbe74@gmail.com>
2025-03-21 16:32:34 -0500

    Initial Commit

---
+++ gitWorkshop.c
@@ -0,0 +1,21 @@
+//sum and add two numbers
+
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sum;
+
+    // Prompt user for input
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    // Calculate sum
+    sum = num1 + num2;
+
+    // Display result
+    printf("The sum of %d and %d is %d\n", num1, num2, sum);
+
+    return 0;
+}
